09bf2bfeb40e56823965b3ef58804aba676434c6
digitalysin <digitalysin@yahoo.co.id>
2012-09-25 17:05:51 +0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'mysql2'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'spork'
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'shoulda-matchers'
+end
+gem 'jquery-rails'
+gem 'slim-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveReport::Application.config.secret_token = 'df4f53dfb3983a1f67e544276e0b2296ca9beb37257bcdc0ac10873c4feb077cbbdad8f61cb37f78bc97bd5e127e1506fd25de4cc4375f2cbc88e851a924410f'
